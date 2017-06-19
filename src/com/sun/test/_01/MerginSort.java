4796238b82e15e67a601dbc3131237f02d1cfe44
sunleic <sunleic@yonyou.com>
2017-06-19 11:31:24 +0800

    二分查找算法

--- src/com/sun/test/_01/MerginSort.java
+++ src/com/sun/test/_01/MerginSort.java
@@ -5,6 +5,7 @@ package com.sun.test._01;
  * 基本排序：归并（Merge）排序法是将两个（或两个以上）有序表合并成一个新的有序表，
  * 即把待排序序列分为若干个子序列，每个子序列是有序的。
  * 然后再把有序子序列合并为整体有序序列。
+ * 时间复杂度：O(nlog2n),稳定
  */
 public class MerginSort {
     public MerginSort(int[] a) {
