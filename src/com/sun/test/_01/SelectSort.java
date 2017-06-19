d1f00f9b5f91121572a76be9cf7c69ef15675fae
sunleic <sunleic@yonyou.com>
2017-06-19 10:50:22 +0800

    二分查找算法

--- src/com/sun/test/_01/SelectSort.java
+++ src/com/sun/test/_01/SelectSort.java
@@ -4,6 +4,7 @@ package com.sun.test._01;
  * 简单选择排序
  * 基本思想：在要排序的一组数中，选出最小的一个数与第一个位置的数交换；
  * 然后在剩下的数当中再找最小的与第二个位置的数交换，如此循环到倒数第二个数和最后一个数比较为止。
+ * 时间复杂度：O(n^2),不稳定
  */
 public class SelectSort {
     public SelectSort(int[] a) {
