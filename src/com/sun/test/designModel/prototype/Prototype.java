de4cb5906826a6a2159fdbfd9d73589ddb508f0a
sunleic <sunleic@yonyou.com>
2017-07-18 17:11:19 +0800

    原型模式：就是一个对象的复制

---
+++ src/com/sun/test/designModel/prototype/Prototype.java
@@ -0,0 +1,15 @@
+package com.sun.test.designModel.prototype;
+
+/**
+ * 原型模式（Prototype）
+ * <p>
+ * 原型模式虽然是创建型的模式，但是与工程模式没有关系，从名字即可看出，该模式的思想就是将一个对象作为原型，
+ * 对其进行复制、克隆，产生一个和原对象类似的新对象。本小结会通过对象的复制，进行讲解。在Java中，复制对象是通过clone()实现的
+ * Created by sunleic on 2017/7/18.
+ */
+public class Prototype implements Cloneable {
+    public Object clone() throws CloneNotSupportedException {
+        Prototype prototype = (Prototype) super.clone();
+        return prototype;
+    }
+}
