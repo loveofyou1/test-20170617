a90430fca410a8ae39e2761d6436b16ab2defd57
sunleic <sunleic@yonyou.com>
2017-07-20 09:41:00 +0800

    原型模式对象类增加

---
+++ src/com/sun/test/designModel/prototype/SerializableObject.java
@@ -0,0 +1,10 @@
+package com.sun.test.designModel.prototype;
+
+import java.io.Serializable;
+
+/**
+ * Created by sunleic on 2017/7/19.
+ */
+public class SerializableObject implements Serializable {
+    private static final long serialVersionUID = 3933831370232237069L;
+}
